87c72fc6b6d0c0d35193b3e4dc7e8b68923465b9
Philip Rodrigues <rodriges@fnal.gov>
2021-06-17 10:37:54 +0100

    Add subscribe() call in NetworkObjectReceiver, when appropriate

--- include/nwqueueadapters/NetworkObjectReceiver.hpp
+++ include/nwqueueadapters/NetworkObjectReceiver.hpp
@@ -10,6 +10,7 @@
 #ifndef NWQUEUEADAPTERS_INCLUDE_NWQUEUEADAPTERS_NETWORKOBJECTRECEIVER_HPP_
 #define NWQUEUEADAPTERS_INCLUDE_NWQUEUEADAPTERS_NETWORKOBJECTRECEIVER_HPP_
 
+#include "ipm/Subscriber.hpp"
 #include "nwqueueadapters/networkobjectreceiver/Nljs.hpp"
 #include "nwqueueadapters/networkobjectreceiver/Structs.hpp"
 #include "serialization/Serialization.hpp"
@@ -45,6 +46,12 @@ public:
     : m_receiver(dunedaq::ipm::make_ipm_receiver(conf.ipm_plugin_type))
   {
     m_receiver->connect_for_receives({ { "connection_string", conf.address } });
+
+    // If the Receiver is of Subscriber type, we have to subscribe in order to receive anything
+    auto m_subscriber=std::dynamic_pointer_cast<ipm::Subscriber>(m_receiver);
+    if(m_subscriber){
+      m_subscriber->subscribe(conf.subscription); // Zero-length topic means "subscribe to all"
+    }
   }
 
   T recv(const dunedaq::ipm::Receiver::duration_t& timeout)
--- schema/nwqueueadapters/networkobjectreceiver.jsonnet
+++ schema/nwqueueadapters/networkobjectreceiver.jsonnet
@@ -11,13 +11,17 @@ local nor = {
   ipmtype: s.string("IPMPluginType", doc="IPM plugin type"),
   
   address: s.string("Address", doc="Address to receive from"),
+
+  subscription: s.string("Subscription", doc="Subscription string"),
   
   conf: s.record("Conf",  [
     s.field("ipm_plugin_type", self.ipmtype, "ZmqSender",
       doc="IPM plugin type"),
     s.field("address", self.address, "inproc://default",
-      doc="Address to receive from")
-  ], doc="NetworkObjectReceiver Configuration"),
+      doc="Address to receive from"),
+    s.field("subscription", self.subscription, "",
+      doc="Topic to subscribe to. Empty topic subscribes to everything")
+   ], doc="NetworkObjectReceiver Configuration"),
   
 };
 
